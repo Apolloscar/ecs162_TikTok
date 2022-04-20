6bc7762b76d10ad2517b0ef5ff36ee51fb417f50
Oscar Hernandez <86376444+Apolloscar@users.noreply.github.com>
2022-04-20 23:06:52 +0000

    clean up

--- public/submission.js
+++ public/submission.js
@@ -4,8 +4,6 @@ const report = document.getElementById("video_name");
 
 let nickName = sessionStorage.getItem("pick");
 
-console.log("got",nickName);
-
 let msg = report.textContent;
 msg = msg.replace('userInput', nickName);
 
